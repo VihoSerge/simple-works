36739f26924fc7161e907a394b0e85c3fe346b7b
VihoSerge <komi.vihoserge@gmail.com>
2019-08-31 15:03:39 +0000

    finished react counter app

--- react-simple-counter-app/src/index.js
+++ react-simple-counter-app/src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import './App.css';
 import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component {
+
+    state = {
+      counter: 0
+    }
+  
+    up = () => {
+      this.setState({counter: this.state.counter + 1});
+    }
+  
+    down = () => {
+      if(this.state.counter > 0)
+        this.setState({counter: this.state.counter - 1});
+    }
+  
+    render() {
+      return (
+        <div className="App">
+            <button onClick={this.down} className="counter-btn">-</button>
+            <div className="counter-card card-hover">
+                {this.state.counter}
+            </div>
+            <button onClick={this.up} className="counter-btn">+</button>
+        </div>
+      );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
